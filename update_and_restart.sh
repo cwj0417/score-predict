2d1ce442f2459ae8cbec3ed219ad46cd68ee3d2b
cwj0417 <chenwenjun@yo-cwj.com>
2024-10-25 17:51:57 +0800

    chore: update script

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -2,9 +2,9 @@
 
 git pull
 
-PID=$(lsof -ti :80)
+PID=$(netstat -tulnp | grep :80 | awk '{print $7}' | cut -d'/' -f1)
 if [ -n "$PID" ]; then
   kill -9 $PID
 fi
 
-pnpm build && node .output/server/index.mjs
+pnpm build && export NITRO_PORT="80" && node .output/server/index.mjs &
